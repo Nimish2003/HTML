19b11ebe8723c809fc86c62db5d32c08ba636c60
NimishP08 <113787407+NimishP08@users.noreply.github.com>
2024-03-08 20:42:06 +0530

    add section for work exp

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const name = "Nimish";
     document.getElementById("name").innerHTML = `${name}`;
     document.getElementById("name2").innerHTML = `${name}`;
+    document.getElementById("country").innerHTML = `India`;
