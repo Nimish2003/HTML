e78bb6ad8691e1695f318e03ab2f2995a1c4a2f8
NimishP08 <113787407+NimishP08@users.noreply.github.com>
2024-03-09 00:08:59 +0530

    CSS to home div

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const name = "Nimish";
+const country = "India";
+
     document.getElementById("name").innerHTML = `${name}`;
-    document.getElementById("name2").innerHTML = `${name}`;
-    document.getElementById("country").innerHTML = `India`;
+    document.getElementById("profession").innerHTML = `Web Developer`;
+    document.getElementById("description").innerHTML = `Full stack developer from India`;
